f4b5840983493fa7de04ca7d069339d9ef2b3db8
JOBFER1 <jobfer@outlook.es>
2024-10-19 21:14:35 +0200

    Algo sigue mal

--- src/curso/Principal.java
+++ src/curso/Principal.java
@@ -10,6 +10,7 @@ public class Principal {
 		System.out.println("Sigue todo bien");
 		System.out.println("Algo va mal");
 		System.out.println(9/0);
+		System.out.println("Algo sigue mal");
 	}
 
 }
